f3022c0ea3eae2568819772c88b9bb861402e2da
erwin snijder <erwinsnijder_4@hotmail.com>
2019-05-21 14:23:48 +0200

    chartjs update

--- chartjs.js
+++ chartjs.js
@@ -1,44 +1,64 @@
-var years =        [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
-var africa =       [86,114,106,106,107,111,133,221,783,2478];
-var asia =         [282,350,411,502,635,809,947,1402,3700,5267];
-var europe =       [168,170,178,190,203,276,408,547,675,73];
-var latinamerica = [40,20,10,16,24,38,74,167,508,784];
-var northamerica = [6,3,2,2,7,26,82,172,312,433];
+var Years =        [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
+var World =        [585,660,710,791,978,1262,1650,2521,6008,9725];
+var Africa =       [86,114,106,106,107,111,133,221,783,2478];
+var Asia =         [282,350,411,502,635,809,947,1402,3700,5267];
+var Europe =       [168,170,178,190,203,276,408,547,675,73];
+var Latinamerica = [40,20,10,16,24,38,74,167,508,784];
+var Northamerica = [6,3,2,2,7,26,82,172,312,433];
+var Oceania =      [3,3,3,2,2,2,6,13,30,57];
 
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: years,
+    labels: Years,
     datasets: [
-    { 
-    data: africa,
+      { 
+        data: World,
+        label: "World",
+        borderColor: "#000000",
+        fill: false
+        },
+      { 
+    data: Africa,
+    label: "Africa",
+    borderColor: "#3e95cd",
+    fill: false
+    },
+      { 
+    data: Africa,
     label: "Africa",
     borderColor: "#3e95cd",
     fill: false
     },
     {
-      data: asia,
+      data: Asia,
       label: "Asia",
       borderColor: "#ffff00",
       fill: false 
     },
     { 
-      data: europe,
+      data: Europe,
       label: "Europe",
       borderColor: "#228B22",
       fill: false
     },
     { 
-      data: latinamerica,
-      label: "latinamerica",
+      data: Latinamerica,
+      label: "Latinamerica",
       borderColor: "#FF7F50",
       fill: false
     },
     { 
-      data: northamerica,
+      data: Northamerica,
       label: "North America",
       borderColor: "#ff0000",
+      fill: false
+    },
+    { 
+      data: Oceania,
+      label: "Oceania",
+      borderColor: "#00ffff",
       fill: false
     }
     ]
